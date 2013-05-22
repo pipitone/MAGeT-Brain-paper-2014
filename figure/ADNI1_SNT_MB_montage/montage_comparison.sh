682040ab2e5e60d402f3bed63ef2684fe0193a20
Jon Pipitone <jon.pipitone@gmail.com>
2013-05-22 10:34:24 -0400

    Updated montage to zoom in on the hippocampus

--- figure/ADNI1_SNT_MB_montage/montage_comparison.sh
+++ figure/ADNI1_SNT_MB_montage/montage_comparison.sh
@@ -15,9 +15,11 @@ autocrop+ -byte -keep_real_range -nearest_neighbour -isostep $step $tmp/_ordered
 
 autocrop -isostep $step $ana $tmp/isostep_ana.mnc 
 
-mincreshape $tmp/isostep_lbl.mnc $tmp/cropped_lbl.mnc -start 16,64,52 -count 98,98,98 
-mincreshape $tmp/isostep_cmp.mnc $tmp/cropped_cmp.mnc -start 16,64,52 -count 98,98,98 
-mincreshape $tmp/isostep_ana.mnc $tmp/cropped_ana.mnc  -start 16,64,52 -count 98,98,98 
+window="-start 16,64,52 -count 98,98,98"
+window="-start 40,90,52 -count 50,50,98"
+mincreshape $tmp/isostep_lbl.mnc $tmp/cropped_lbl.mnc $window
+mincreshape $tmp/isostep_cmp.mnc $tmp/cropped_cmp.mnc $window
+mincreshape $tmp/isostep_ana.mnc $tmp/cropped_ana.mnc $window
 
 mincpik -sagittal -auto_range -slice ${slice} $tmp/cropped_ana.mnc $tmp/ana_${slice}_sagittal.png 
 mincpik -sagittal -lookup -spectral -image_range 0 20 -slice ${slice} $tmp/cropped_lbl.mnc $tmp/lbl_${slice}_sagittal.png 
@@ -26,7 +28,7 @@ mincpik -sagittal -lookup -spectral -image_range 0 20 -slice ${slice} $tmp/cropp
 composite -dissolve 55 -tile -gravity south $tmp/ana_${slice}_sagittal.png $tmp/lbl_${slice}_sagittal.png $tmp/lbl_overlay_${slice}_sagittal.png 
 composite -dissolve 55 -tile -gravity south $tmp/ana_${slice}_sagittal.png $tmp/cmp_${slice}_sagittal.png $tmp/cmp_overlay_${slice}_sagittal.png 
 
-montage -geometry 200x200 -tile 3x1 -background black $tmp/ana_${slice}_sagittal.png \
-                                                      $tmp/lbl_overlay_${slice}_sagittal.png \
-                                                      $tmp/cmp_overlay_${slice}_sagittal.png  $out
+montage -tile x1 -geometry +2+2 -background black $tmp/ana_${slice}_sagittal.png \
+                                   $tmp/lbl_overlay_${slice}_sagittal.png \
+                                   $tmp/cmp_overlay_${slice}_sagittal.png  $out
 echo $tmp
